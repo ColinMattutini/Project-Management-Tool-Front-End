f71a5e8339a951fe7e980243edb94ff4437ef1df
Colin <cmmatt14@gmail.com>
2022-12-09 17:43:37 -0500

    pass down what project is currently being viewed

--- project-management-tool/src/components/ProjectTracker/ProjectDashboard.js
+++ project-management-tool/src/components/ProjectTracker/ProjectDashboard.js
@@ -1,18 +1,23 @@
-import * as React from "react";
+import React, { Fragment } from "react";
 import BacklogColumn from "./TaskColumns/BacklogColumn";
 import CompleteColumn from "./TaskColumns/CompleteColumn";
 import InProgressColumn from "./TaskColumns/InProgressColumn";
 import classes from "./ProjectDashboard.module.css";
 import ReviewColumn from "./TaskColumns/ReviewColumn";
+import SideNavBar from "../SideNavBar/SideNavBar";
+import { useParams } from "react-router-dom";
 
 const ProjectDashboard = () => {
+  let { project } = useParams();
   return (
-    <div className={classes.column}>
-      <BacklogColumn />
-      <InProgressColumn />
-      <ReviewColumn />
-      <CompleteColumn />
-    </div>
+    <Fragment>
+      <div className={classes.column}>
+        <BacklogColumn project={project} />
+        <InProgressColumn />
+        <ReviewColumn />
+        <CompleteColumn />
+      </div>
+    </Fragment>
   );
 };
 
