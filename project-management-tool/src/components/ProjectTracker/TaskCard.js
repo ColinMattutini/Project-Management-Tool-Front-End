204de6f48a3157ff23c42ccef716cb71bf17f2bb
Colin <cmmatt14@gmail.com>
2022-12-09 17:42:32 -0500

    task card to get task information passed down

--- project-management-tool/src/components/ProjectTracker/TaskCard.js
+++ project-management-tool/src/components/ProjectTracker/TaskCard.js
@@ -3,22 +3,40 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { useState } from "react";
+import IndividualTask from "./TaskColumns/IndividualTask/IndividualTask";
 
-const TaskCard = () => {
+const TaskCard = (props) => {
+  const [showTask, setShowTask] = useState(false);
+
+  const taskModalHandler = () => {
+    showTask ? setShowTask(false) : setShowTask(true);
+  };
+
   return (
     <div>
-      <Card sx={{ minWidth: 275 }}>
+      {showTask && (
+        <IndividualTask
+          taskModalHandler={taskModalHandler}
+          taskName={props.taskName}
+          taskType={props.taskType}
+          dateCreated={props.dateCreated}
+          assignedTo={props.assignedTo}
+          status={props.status}
+        />
+      )}
+      <Card sx={{ minWidth: 275 }} onClick={taskModalHandler}>
         <CardContent>
           <Typography variant="h6" component="div">
-            Task Name
+            {props.taskName}
           </Typography>
           <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            TASK TYPE
+            {props.taskType}
           </Typography>
           <Typography variant="body2">
-            Date Created: ...
+            {props.dateCreated}
             <br />
-            Assigned To
+            {props.assignedTo}
           </Typography>
         </CardContent>
       </Card>
