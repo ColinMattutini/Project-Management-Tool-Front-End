0c117906c13e0bf96d5a024a09bbaaced0d9afa5
Colin <cmmatt14@gmail.com>
2022-12-09 17:42:06 -0500

    temp data added to pass to task cards

--- project-management-tool/src/components/ProjectTracker/TaskColumns/BacklogColumn.js
+++ project-management-tool/src/components/ProjectTracker/TaskColumns/BacklogColumn.js
@@ -2,16 +2,71 @@ import React, { Fragment, useState } from "react";
 import classes from "./BacklogColumn.module.css";
 import TaskCard from "../TaskCard";
 import NewTask from "./NewTask/NewTask";
-import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
 import { IconButton } from "@mui/material";
 
-const BacklogColumn = () => {
+const backlogItems = [
+  {
+    postId: 1,
+    projectId: "fitnesstracker",
+    taskName: "test",
+    taskType: "Moderate",
+    dateCreated: "12/07/2022",
+    assignedTo: "TestUser",
+    status: "Backlog",
+  },
+  {
+    postId: 2,
+    projectId: "forumapplication",
+    taskName: "Luke Skywalker",
+    taskType: "Low",
+    dateCreated: "12/07/2022",
+    assignedTo: "TestUser",
+    status: "Backlog",
+  },
+  {
+    postId: 3,
+    projectId: "forumapplication",
+    taskName: "Add favorite topics section",
+    taskType: "Low",
+    dateCreated: "12/07/2022",
+    assignedTo: "TestUser",
+    status: "Backlog",
+  },
+  {
+    postId: 4,
+    projectId: "forumapplication",
+    taskName: "Fix search bar dropdowns",
+    taskType: "Low",
+    dateCreated: "12/07/2022",
+    assignedTo: "TestUser",
+    status: "Backlog",
+  },
+];
+
+const BacklogColumn = (props) => {
   const [newTaskModal, setNewTaskModal] = useState(false);
+  const [tasks, setTasks] = useState([]);
 
   const newTaskModalHandler = () => {
     newTaskModal ? setNewTaskModal(false) : setNewTaskModal(true);
+    console.log(props.project);
   };
+
+  const newBackLogItems = backlogItems.filter((e) =>
+    e.projectId.includes(props.project)
+  );
+  const backlogTasks = newBackLogItems.map((backlogItems) => (
+    <TaskCard
+      key={backlogItems.postId}
+      projectId={backlogItems.projectId}
+      taskName={backlogItems.taskName}
+      taskType={backlogItems.taskType}
+      dateCreated={backlogItems.dateCreated}
+      assignedTo={backlogItems.assignedTo}
+      status={backlogItems.status}
+    />
+  ));
 
   return (
     <Fragment>
@@ -28,7 +83,7 @@ const BacklogColumn = () => {
             <AddIcon />
           </IconButton>
         </div>
-        <TaskCard />
+        {backlogTasks}
       </div>
     </Fragment>
   );
