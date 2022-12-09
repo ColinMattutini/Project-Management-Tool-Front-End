80482e889c29d84ed440c43c28c7df6264155700
Colin <cmmatt14@gmail.com>
2022-12-09 17:44:20 -0500

    individual task modal to edit a task

---
+++ project-management-tool/src/components/ProjectTracker/TaskColumns/IndividualTask/IndividualTask.js
@@ -0,0 +1,116 @@
+import React, { Fragment, useState } from "react";
+import Modal from "../../../UI/Modal";
+import EditIcon from "@mui/icons-material/Edit";
+import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
+import Autocomplete from "@mui/material/Autocomplete";
+import classes from "./IndividualTask.module.css";
+import { Button } from "@mui/material";
+
+const taskPriority = [
+  {
+    value: "Low",
+    label: "Low",
+  },
+  {
+    value: "Moderate",
+    label: "Moderate",
+  },
+  {
+    value: "High",
+    label: "High",
+  },
+  {
+    value: "Urgent",
+    label: "Urgent",
+  },
+];
+
+const users = [
+  {
+    label: "Colin",
+  },
+  {
+    label: "Test User",
+  },
+  {
+    label: "Jack Sparrow",
+  },
+  {
+    label: "Luke Skywalker",
+  },
+  {
+    label: "Mahogany Teakwood",
+  },
+];
+
+const IndividualTask = (props) => {
+  const [priority, setPriority] = useState(props.taskType);
+  const [taskName, setTaskName] = useState(props.taskName);
+  const [assignedTo, setAssignedTo] = useState(props.assignedTo);
+
+  const handleChange = (event) => {
+    setPriority(event.target.value);
+  };
+
+  const taskNameHandler = (e) => {
+    setTaskName(e.target.value);
+  };
+
+  const submitUpdateTask = (e) => {
+    e.preventDefault();
+    console.log(priority, taskName, assignedTo);
+  };
+
+  const assignedHandler = (e) => {
+    setAssignedTo(e.target.value);
+  };
+  return (
+    <Modal>
+      <button onClick={props.taskModalHandler}>X</button>
+      <form className={classes.alignment}>
+        <h1>Task Name</h1>
+        <input value={taskName} onChange={taskNameHandler}></input>
+        <input placeholder="Additional Information"></input>
+        <TextField
+          id="outlined-select-priority"
+          select
+          value={priority}
+          onChange={handleChange}
+          helperText="Task Priority"
+        >
+          {taskPriority.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={users}
+          sx={{ width: 300 }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Assigned To"
+              onChange={assignedHandler}
+              value={assignedTo}
+            />
+          )}
+        />
+        <div className={classes.buttonSizer}>
+          <Button
+            variant="contained"
+            color="success"
+            onClick={submitUpdateTask}
+          >
+            Save Task
+          </Button>
+        </div>
+      </form>
+    </Modal>
+  );
+};
+
+export default IndividualTask;
