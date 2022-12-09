b7e8cef7021162ee7715215ffb931708f7b6daa5
Colin <cmmatt14@gmail.com>
2022-12-09 17:40:53 -0500

    added page routing

--- project-management-tool/src/App.js
+++ project-management-tool/src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import ProjectDashboard from './components/ProjectTracker/ProjectDashboard';
+import "./App.css";
+import ProjectDashboard from "./components/ProjectTracker/ProjectDashboard";
+import SideNavBar from "./components/SideNavBar/SideNavBar";
+import MembersPage from "./Pages/MembersPage";
+import { Route, Routes } from "react-router-dom";
+import UserTasksPage from "./Pages/UserTasksPage";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
     <div>
-      <ProjectDashboard />
+      <SideNavBar />
+      <Routes>
+        <Route exact path="/*" element={<HomePage />} />
+        <Route exact path="/Projects/:project" element={<ProjectDashboard />} />
+        <Route exact path="/members" element={<MembersPage />} />
+        <Route exact path="/usertasks" element={<UserTasksPage />} />
+      </Routes>
     </div>
   );
 }
