d4eb95ef75b065f55559b4087b820f9332113511
Colin <cmmatt14@gmail.com>
2022-12-16 17:04:28 -0500

    added function to save task to database

--- project-management-tool/src/components/ProjectTracker/TaskColumns/NewTask/NewTask.js
+++ project-management-tool/src/components/ProjectTracker/TaskColumns/NewTask/NewTask.js
@@ -5,23 +5,24 @@ import MenuItem from "@mui/material/MenuItem";
 import Autocomplete from "@mui/material/Autocomplete";
 import classes from "./NewTask.module.css";
 import { Button } from "@mui/material";
+import { json } from "react-router-dom";
 
 const taskPriority = [
   {
-    value: "Low",
-    label: "Low",
+    value: "LOW",
+    label: "LOW",
   },
   {
-    value: "Moderate",
-    label: "Moderate",
+    value: "MODERATE",
+    label: "MODERATE",
   },
   {
-    value: "High",
-    label: "High",
+    value: "HIGH",
+    label: "HIGH",
   },
   {
-    value: "Urgent",
-    label: "Urgent",
+    value: "URGENT",
+    label: "URGENT",
   },
 ];
 
@@ -41,13 +42,81 @@ const users = [
   {
     label: "Mahogany Teakwood",
   },
+  {
+    label: "",
+  },
+];
+
+const taskCategory = [
+  {
+    value: "BACKLOG",
+    label: "BACKLOG",
+  },
+  {
+    value: "INPROGRESS",
+    label: "INPROGRESS",
+  },
+  {
+    value: "REVIEW",
+    label: "REVIEW",
+  },
+  {
+    value: "COMPLETE",
+    label: "COMPLETE",
+  },
 ];
 
 const NewTask = (props) => {
-  const [priority, setPriority] = useState("Low");
+  const pathName = window.location.pathname.split("/").pop();
+
+  const [priority, setPriority] = useState("LOW");
+  const [taskName, setTaskName] = useState();
+  const [additionalInfo, setAdditionalInfo] = useState();
+  const [assignedTo, setAssignedTo] = useState("");
+  const [category, setCategory] = useState("BACKLOG");
 
   const handleChange = (event) => {
     setPriority(event.target.value);
+  };
+
+  const taskNameHandler = (e) => {
+    setTaskName(e.target.value);
+  };
+
+  const additionalInfoHandler = (event) => {
+    setAdditionalInfo(event.target.value);
+  };
+
+  const categoryHandler = (e) => {
+    setCategory(e.target.value);
+  };
+
+  const submitNewTask = (e) => {
+    e.preventDefault();
+    saveTaskFetch();
+    console.log(priority, taskName, assignedTo);
+    props.newTaskModalHandler();
+  };
+
+  const assignedHandler = (e) => {
+    setAssignedTo(e.target.value);
+  };
+
+  const saveTaskFetch = async () => {
+    const response = await fetch(
+      "http://localhost:8080/api/project/" + pathName + "/newtask",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          taskName: taskName,
+          additionalInfo: additionalInfo,
+          priority: priority,
+          assignedTo: assignedTo,
+          category: category,
+        }),
+        headers: { "content-type": "application/json" },
+      }
+    );
   };
 
   return (
@@ -55,8 +124,11 @@ const NewTask = (props) => {
       <button onClick={props.newTaskModalHandler}>X</button>
       <form className={classes.alignment}>
         <h1>Task Name</h1>
-        <input placeholder="Task Name"></input>
-        <input placeholder="Additional Information"></input>
+        <input placeholder="Task Name" onChange={taskNameHandler}></input>
+        <input
+          placeholder="Additional Information"
+          onChange={additionalInfoHandler}
+        ></input>
         <TextField
           id="outlined-select-priority"
           select
@@ -71,16 +143,39 @@ const NewTask = (props) => {
           ))}
         </TextField>
         <Autocomplete
+          value={assignedTo}
+          isOptionEqualToValue={(option, value) => option.id === value.id}
           disablePortal
           id="combo-box-demo"
           options={users}
           sx={{ width: 300 }}
+          onInputChange={(event, newInputValue) => {
+            setAssignedTo(newInputValue);
+          }}
+          inputValue={assignedTo}
           renderInput={(params) => (
-            <TextField {...params} label="Assigned To" />
+            <TextField
+              {...params}
+              label="Assigned To"
+              onChange={assignedHandler}
+              value={assignedTo}
+            />
           )}
         />
+        <TextField
+          id="outlined-select-category"
+          select
+          value={category}
+          onChange={categoryHandler}
+        >
+          {taskCategory.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
         <div className={classes.buttonSizer}>
-          <Button variant="contained" color="success">
+          <Button variant="contained" color="success" onClick={submitNewTask}>
             Submit Task
           </Button>
         </div>
