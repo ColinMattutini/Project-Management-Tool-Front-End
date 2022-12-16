1431b61125bca052bc3ae9e5a7221d4945620cce
Colin <cmmatt14@gmail.com>
2022-12-15 19:08:46 -0500

    fetch all tasks by project function added

--- project-management-tool/src/components/ProjectTracker/ProjectDashboard.js
+++ project-management-tool/src/components/ProjectTracker/ProjectDashboard.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import BacklogColumn from "./TaskColumns/BacklogColumn";
 import CompleteColumn from "./TaskColumns/CompleteColumn";
 import InProgressColumn from "./TaskColumns/InProgressColumn";
@@ -8,14 +8,26 @@ import SideNavBar from "../SideNavBar/SideNavBar";
 import { useParams } from "react-router-dom";
 
 const ProjectDashboard = () => {
+  const [backlogTasks, setBacklogTasks] = useState([]);
+
+  const fetchAllTasks = async () => {
+    const response = await fetch("http://localhost:8080/api/project/2/tasks", {
+      method: "GET",
+    });
+    const data = await response.json();
+    var backlog = data.filter((e) => (e.category = "BACKLOG"));
+    setBacklogTasks(backlog);
+  };
+
   let { project } = useParams();
   return (
     <Fragment>
       <div className={classes.column}>
-        <BacklogColumn project={project} />
+        <BacklogColumn project={project} backlogTasks={backlogTasks} />
         <InProgressColumn />
         <ReviewColumn />
         <CompleteColumn />
+        <button onClick={fetchAllTasks}>FEtch</button>
       </div>
     </Fragment>
   );
