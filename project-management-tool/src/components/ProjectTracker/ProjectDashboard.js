905a4872413e7510b58d970b2ca70ded180dbcd5
Colin <cmmatt14@gmail.com>
2022-12-16 17:03:50 -0500

    get all tasks and pass down to correct category

--- project-management-tool/src/components/ProjectTracker/ProjectDashboard.js
+++ project-management-tool/src/components/ProjectTracker/ProjectDashboard.js
@@ -1,33 +1,54 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import BacklogColumn from "./TaskColumns/BacklogColumn";
 import CompleteColumn from "./TaskColumns/CompleteColumn";
 import InProgressColumn from "./TaskColumns/InProgressColumn";
 import classes from "./ProjectDashboard.module.css";
 import ReviewColumn from "./TaskColumns/ReviewColumn";
 import SideNavBar from "../SideNavBar/SideNavBar";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 
 const ProjectDashboard = () => {
   const [backlogTasks, setBacklogTasks] = useState([]);
+  const [inProgressTasks, setInProgressTaks] = useState([]);
+  const [reviewTasks, setReviewTasks] = useState([]);
+  const [completeTasks, setCompleteTasks] = useState([]);
+  const location = useLocation();
+  const pathName = window.location.pathname.split("/").pop();
 
   const fetchAllTasks = async () => {
-    const response = await fetch("http://localhost:8080/api/project/2/tasks", {
-      method: "GET",
-    });
+    console.log(pathName);
+    const response = await fetch(
+      "http://localhost:8080/api/project/" + pathName + "/tasks",
+      {
+        method: "GET",
+      }
+    );
     const data = await response.json();
-    var backlog = data.filter((e) => (e.category = "BACKLOG"));
+
+    var complete = data.filter((e) => e.category === "COMPLETE");
+    var review = data.filter((e) => e.category === "REVIEW");
+    var backlog = data.filter((e) => e.category === "BACKLOG");
+    var inProgress = data.filter((e) => e.category === "INPROGRESS");
+
     setBacklogTasks(backlog);
+    setInProgressTaks(inProgress);
+    setReviewTasks(review);
+    setCompleteTasks(complete);
   };
 
   let { project } = useParams();
+
+  useEffect(() => {
+    fetchAllTasks();
+    console.log(pathName);
+  }, [pathName]);
   return (
     <Fragment>
       <div className={classes.column}>
         <BacklogColumn project={project} backlogTasks={backlogTasks} />
-        <InProgressColumn />
-        <ReviewColumn />
-        <CompleteColumn />
-        <button onClick={fetchAllTasks}>FEtch</button>
+        <InProgressColumn project={project} inProgressTasks={inProgressTasks} />
+        <ReviewColumn project={project} reviewTasks={reviewTasks} />
+        <CompleteColumn project={project} completeTasks={completeTasks} />
       </div>
     </Fragment>
   );
