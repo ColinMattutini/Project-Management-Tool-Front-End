67347a242c8a371033f672594064d7e63cae1144
Colin <cmmatt14@gmail.com>
2022-12-18 17:09:26 -0500

    in progress and review tasks added

--- project-management-tool/src/components/ProjectTracker/TaskColumns/InProgressColumn.js
+++ project-management-tool/src/components/ProjectTracker/TaskColumns/InProgressColumn.js
@@ -2,14 +2,24 @@ import React from "react";
 import TaskCard from "../TaskCard";
 import classes from "./InProgressColumn.module.css";
 
-const InProgressColumn = () => {
+const InProgressColumn = (props) => {
+  const inProgressTasks = props.inProgressTasks.map((inProgressItems) => (
+    <TaskCard
+      key={inProgressItems.taskId}
+      taskId={inProgressItems.taskId}
+      projectId={inProgressItems.projectId}
+      taskName={inProgressItems.taskName}
+      additionalInfo={inProgressItems.additionalInfo}
+      taskType={inProgressItems.priority}
+      dateCreated={inProgressItems.dateCreated}
+      assignedTo={inProgressItems.assignedTo}
+      status={inProgressItems.category}
+    />
+  ));
   return (
     <div className={classes.column}>
       <h1>In-Progress</h1>
-      <TaskCard />
-      <TaskCard />
-      <TaskCard />
-      <TaskCard />
+      {inProgressTasks}
     </div>
   );
 };
--- project-management-tool/src/components/ProjectTracker/TaskColumns/ReviewColumn.js
+++ project-management-tool/src/components/ProjectTracker/TaskColumns/ReviewColumn.js
@@ -2,14 +2,24 @@ import React from "react";
 import TaskCard from "../TaskCard";
 import classes from "./ReviewColumn.module.css";
 
-const ReviewColumn = () => {
+const ReviewColumn = (props) => {
+  const reviewTasks = props.reviewTasks.map((reviewItems) => (
+    <TaskCard
+      key={reviewItems.taskId}
+      taskId={reviewItems.taskId}
+      projectId={reviewItems.projectId}
+      taskName={reviewItems.taskName}
+      additionalInfo={reviewItems.additionalInfo}
+      taskType={reviewItems.priority}
+      dateCreated={reviewItems.dateCreated}
+      assignedTo={reviewItems.assignedTo}
+      status={reviewItems.category}
+    />
+  ));
   return (
     <div className={classes.column}>
       <h1>Review</h1>
-      <TaskCard />
-      <TaskCard />
-      <TaskCard />
-      <TaskCard />
+      {reviewTasks}
     </div>
   );
 };
