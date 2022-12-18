70ecfd65ff596c2a6819679ee7eda470f5e6a034
Colin <cmmatt14@gmail.com>
2022-12-18 17:08:45 -0500

    complete tasks added to pass to task card

--- project-management-tool/src/components/ProjectTracker/TaskColumns/CompleteColumn.js
+++ project-management-tool/src/components/ProjectTracker/TaskColumns/CompleteColumn.js
@@ -2,14 +2,25 @@ import React from "react";
 import TaskCard from "../TaskCard";
 import classes from "./CompleteColumn.module.css";
 
-const CompleteColumn = () => {
+const CompleteColumn = (props) => {
+  const completeTasks = props.completeTasks.map((completeItems) => (
+    <TaskCard
+      key={completeItems.taskId}
+      taskId={completeItems.taskId}
+      projectId={completeItems.projectId}
+      taskName={completeItems.taskName}
+      additionalInfo={completeItems.additionalInfo}
+      taskType={completeItems.priority}
+      dateCreated={completeItems.dateCreated}
+      assignedTo={completeItems.assignedTo}
+      status={completeItems.category}
+    />
+  ));
+
   return (
     <div className={classes.column}>
       <h1>Complete</h1>
-      <TaskCard />
-      <TaskCard />
-      <TaskCard />
-      <TaskCard />
+      {completeTasks}
     </div>
   );
 };
