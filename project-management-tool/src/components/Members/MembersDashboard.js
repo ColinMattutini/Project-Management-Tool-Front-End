aa57aa9cae2a1bebcd6b045d5518893b39bbea26
Colin <cmmatt14@gmail.com>
2022-12-20 19:06:40 -0500

    button added to open and close new member form

--- project-management-tool/src/components/Members/MembersDashboard.js
+++ project-management-tool/src/components/Members/MembersDashboard.js
@@ -1,12 +1,34 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import MembersTable from "./MembersTable";
 import classes from "./MembersDashBoard.module.css";
+import { Button } from "@mui/material";
+import NewMemberForm from "./NewMember/NewMemberForm";
 
 const MembersDashBoard = () => {
+  const [showNewMemberModal, setShowNewMemberModal] = useState(false);
+
+  const newMemberModalHandler = () => {
+    showNewMemberModal
+      ? setShowNewMemberModal(false)
+      : setShowNewMemberModal(true);
+  };
+
   return (
-    <div className={classes.column}>
-      <MembersTable />
-    </div>
+    <Fragment>
+      {showNewMemberModal && (
+        <NewMemberForm newMemberModalHandler={newMemberModalHandler} />
+      )}
+      <div className={classes.column}>
+        <Button
+          variant="contained"
+          color="success"
+          onClick={newMemberModalHandler}
+        >
+          Add Member
+        </Button>
+        <MembersTable />
+      </div>
+    </Fragment>
   );
 };
 
