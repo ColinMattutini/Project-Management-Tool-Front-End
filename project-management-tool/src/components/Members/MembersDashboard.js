aecd7cf36e88d003d51fea66d774b6001606eca6
Colin <cmmatt14@gmail.com>
2023-01-12 19:40:14 -0500

    logic for user to be logged in to add member

--- project-management-tool/src/components/Members/MembersDashboard.js
+++ project-management-tool/src/components/Members/MembersDashboard.js
@@ -1,11 +1,13 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useContext, useState } from "react";
 import MembersTable from "./MembersTable";
 import classes from "./MembersDashBoard.module.css";
 import { Button } from "@mui/material";
 import NewMemberForm from "./NewMember/NewMemberForm";
+import AuthContext from "../../context/AuthContext";
 
 const MembersDashBoard = () => {
   const [showNewMemberModal, setShowNewMemberModal] = useState(false);
+  const authCtx = useContext(AuthContext);
 
   const newMemberModalHandler = () => {
     showNewMemberModal
@@ -19,13 +21,15 @@ const MembersDashBoard = () => {
         <NewMemberForm newMemberModalHandler={newMemberModalHandler} />
       )}
       <div className={classes.column}>
-        <Button
-          variant="contained"
-          color="success"
-          onClick={newMemberModalHandler}
-        >
-          Add Member
-        </Button>
+        {authCtx.isLoggedIn && (
+          <Button
+            variant="contained"
+            color="success"
+            onClick={newMemberModalHandler}
+          >
+            Add Member
+          </Button>
+        )}
         <MembersTable />
       </div>
     </Fragment>
